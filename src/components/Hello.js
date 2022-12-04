a37e1a8e4309b5518fa018da0f05580efafa1ec6
Conor Dunne <conordunne@outlook.ie>
2022-12-05 05:49:44 +0900

    Add Hello component

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div className = "dummyClass">
+            <h1>Hello Conor</h1>
+        </div>
+    )
+}
+
+export default Hello
