4e5cfe7dea31a31620fead36696c7588def9f4a3
Conor Dunne <conordunne@outlook.ie>
2022-12-05 06:55:09 +0900

    Add some test props

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,13 @@
 import React from "react";
+import './myStyle.css';
+
 
 const Hello = () => {
-    return (
-        <div className = "dummyClass">
-            <h1>Hello Conor</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="hello">Hello Conor</h1>
+    </div>
+  )
 }
 
-export default Hello
+export default Hello;
