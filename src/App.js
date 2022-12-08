80f2dfe0dd6312ae96668b3eba2d1135c8bf5627
Conor <conordunne@outlook.ie>
2022-12-08 02:28:28 +0000

    Add Welcome.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
+      <Message/>
     </div>
   );
 }
