959f0a48ac1bffa5b4eeaa958040c857f41e6eab
Conor <conordunne@outlook.ie>
2022-12-17 01:57:50 +0000

    Add EventBind function

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import Hello from './components/Hello';
 import Stylesheet from './components/Stylesheet';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-    <Counter/>
+      <EventBind />
+      {/* <FunctionClick />
+      <ClassClick /> */}
+    {/* <Counter/>
       <Stylesheet/>
       <Greet name="Bruce" heroName="Batman">
         <p>This is child paragraph.</p>
@@ -23,7 +29,7 @@ function App() {
       <Greet name="Diana" heroName="Wonder Woman"/>
       <Welcome name="Bruce" heroName="Batman" />
       <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
     </div>
   );
 }
---
+++ src/components/EventBind.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message: 'Hello'
+        }
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler() {
+        console.log("I've been clicked!");
+        this.setState({
+            message: 'Goodbye!'
+        })
+    }
+
+render() {
+    return (
+        <div>
+            <div>{this.state.message}</div>
+            <button onClick={this.clickHandler}>Click</button>
+        </div>
+    )
+}
+
+}
+
+
+export default EventBind
